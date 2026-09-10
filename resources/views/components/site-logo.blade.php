0fce47cf1824e6d4a2f71e2f42731058c213cbc5
subhomoy samanta <subhomoy@keylines.net>
2026-09-10 14:15:02 +0530

    dashboard page section swaping

--- resources/views/components/site-logo.blade.php
+++ resources/views/components/site-logo.blade.php
@@ -1,2 +1,2 @@
 <!-- <img src="{{ $url }}" alt="{{ config('app.name') }}" {{ $attributes }}> -->
-<img src="/public/rotary.jpeg" alt="{{ config('app.name') }}" {{ $attributes }}>
+<img src="/public/images/rotary.jpeg" alt="{{ config('app.name') }}" {{ $attributes }}>
