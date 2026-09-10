4f4404cdf606244b0201a1303376829bc0effcd0
subhomoy samanta <subhomoy@keylines.net>
2026-09-10 15:35:56 +0530

    logo update

--- resources/views/components/site-logo.blade.php
+++ resources/views/components/site-logo.blade.php
@@ -1,3 +1,2 @@
 <!-- <img src="{{ $url }}" alt="{{ config('app.name') }}" {{ $attributes }}> -->
-<img src="/public/images/rotary.jpeg" alt="{{ config('app.name') }}" {{ $attributes }} style="width: 100%;
-    height: 65px;">
+<img src="/public/images/Rotary-logo.png" alt="{{ config('app.name') }}" {{ $attributes }}>
