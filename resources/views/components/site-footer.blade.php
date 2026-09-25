b9a79f43b6a9457227014cc5f3d0c3c2ee075ca7
subhomoy samanta <subhomoy@keylines.net>
2026-09-25 15:07:38 +0530

    client feedback points 24-09-2026

--- resources/views/components/site-footer.blade.php
+++ resources/views/components/site-footer.blade.php
@@ -1,5 +1,14 @@
 <footer class="w-full shrink-0 bg-rotary-navy px-6 py-6">
     <p class="text-center text-xs text-white/50">
         &copy; {{ date('Y') }} Rotary District 3291. All rights reserved.
+        <span class="mt-2 block">
+            Developed by
+            <a
+                href="https://www.brandmanconsultancy.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="font-medium text-white/70 transition hover:text-white hover:underline"
+            >Brandman Consultancy</a>.
+        </span>
     </p>
 </footer>
